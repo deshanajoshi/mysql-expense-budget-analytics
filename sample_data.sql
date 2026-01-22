6a299bfe23e89b7ca82652ed59b62e2d24855d25
Deshna Joshi <joshideshna@gmail.com>
2026-01-22 21:30:21 +0530

    Create sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO users VALUES
+(1,'Deshna');
+
+INSERT INTO categories VALUES
+(1,'Food'),(2,'Travel'),(3,'Shopping');
+
+INSERT INTO expenses VALUES
+(1,1,1,2500,'2024-03-01'),
+(2,1,2,4000,'2024-03-05'),
+(3,1,3,3000,'2024-03-10');
+
+INSERT INTO budgets VALUES
+(1,1,10000,'2024-03');
